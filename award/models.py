3903142148f5e686a7605f51cfa66ded0b0264ec
kiptoo-097 <kiptoo097.com>
2020-12-01 02:53:17 +0300

    Created class profile

--- award/models.py
+++ award/models.py
@@ -5,3 +5,13 @@ from django.utils import timezone
 from django.db.models.signals import post_save
 from tinymce.models import HTMLField
 from cloudinary.models import CloudinaryField
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = CloudinaryField('image')
+    bio = models.TextField(default="No bio!")
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
