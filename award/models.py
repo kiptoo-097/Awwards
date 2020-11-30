807ba4d38c94b7c79549602eef2b2aecdde1b702
kiptoo-097 <kiptoo097.com>
2020-12-01 02:50:08 +0300

    Imported cloudinary

--- award/models.py
+++ award/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-
-
-
+from django.dispatch import receiver
+from django.utils import timezone
+from django.db.models.signals import post_save
+from tinymce.models import HTMLField
+from cloudinary.models import CloudinaryField
