313d2ee358b5414f2c70ce80a0be228f3a06cb35
kiptoo-097 <kiptoo097.com>
2020-12-01 03:06:54 +0300

    Added new post  view method that returns new post template

--- award/views.py
+++ award/views.py
@@ -41,6 +41,19 @@ def project(request):
     return render(request, 'projects.html', locals())
 
 
+@login_required(login_url='/accounts/login/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+        return redirect('project')
+    else:
+        form = NewPostForm()
+    return render(request, 'new_post.html', {"form": form})
 
 
 
