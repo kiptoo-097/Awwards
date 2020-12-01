d2b5f7d1e3d25f09ca8931b74267bca9d508a4d6
kiptoo-097 <kiptoo097.com>
2020-12-01 03:12:54 +0300

    Added forms.py

---
+++ award/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import *
+
+class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
+        model = Profile
+        fields = ('image', 'bio')
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'post_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+        
+class ProjectRatingForm(forms.ModelForm):
+    class Meta:
+        model = Rating
+        exclude = ['project', 'pub_date', 'user']
